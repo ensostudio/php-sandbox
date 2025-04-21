e33b31741119dda178789ad753e527f0a675a9eb
Anton Fedonuk <info@ensostudio.ru>
2025-04-22 01:19:40 +0300

    Fix ExecuteAction

--- src/Actions/ExecuteAction.php
+++ src/Actions/ExecuteAction.php
@@ -2,7 +2,7 @@
 
 namespace EnsoStudio\PhpSandbox\Actions;
 
-use Slim\Exception\HttpInternalServerErrorException;
+use ErrorException;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 use Throwable;
@@ -18,27 +18,46 @@ class ExecuteAction implements ActionInterface
     public function __invoke(Request $request, Response $response): Response
     {
         $code = $request->getParsedBody()['code'];
-        $code = \preg_replace('/^<\?(php)?\r?\n/', '', \trim($code), 1);
+        $code = \preg_replace('/^<\?(php)?\s*\r?\n/', '', \trim($code), 1);
 
         \ob_start();
         try {
-            require __DIR__ . '/../bootstrap.php';
-            eval($code);
+            self::executeCode($code);
             $response->getBody()->write(\ob_get_clean() . '#php-sandbox-end-output#');
         } catch (Throwable $e) {
-            $msg = \get_class($e) . ': ' . \str_replace(['"', "\r", "\n"], ["'", '', ' '], $e->getMessage());
+            $error = \get_class($e) . ': ' . \str_replace(['"', "\r", "\n"], ["'", '', ' '], $e->getMessage());
             $line = $e->getLine() + 1;
-            foreach ($e->getTrace() as $trace) {
-                if (\str_ends_with($trace['file'], 'eval()\'d code')) {
-                    $line = $trace['line'] + 1;
+            foreach ($e->getTrace() as $data) {
+                if (\str_ends_with($data['file'], 'eval()\'d code')) {
+                    $line = $data['line'] + 1;
                     break;
                 }
             }
 
-            $response->getBody()->write(\ob_get_clean() ?: '');
-            $response = $response->withHeader('X-Error', '"' . $msg . '"; line=' . $line)->withStatus(500);
+            $response = $response->withHeader('X-Error', '"' . $error . '"; line=' . $line)
+                ->withStatus(500);
+            $response->getBody()->write(\ob_get_clean());
         }
 
         return $response;
+    }
+
+    /**
+     * Execute code in clear context.
+     */
+    static private function executeCode(string $code)
+    {
+        \error_reporting(E_ALL);
+        \ini_set('log_errors', 'Off');
+        \ini_set('display_errors', 'On');
+        \set_error_handler(
+            static function ($code, $message, $file, $line) {
+                throw new ErrorException($message, $code, $code, $file, $line);
+            }
+        );
+
+        require \dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+        eval($code);
     }
 }
